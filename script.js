3b25f016e65e5abeef32ae279cd6a75b69a310ff
saitg <codersait@gmail.com>
2020-09-21 16:45:34 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let day = prompt('Lutfen bir gun ismi giriniz');
+
+const weekdays = ['pazartesi', 'sali', 'carsamba', 'persembe', 'cuma']
+const weekend = ['cumartesi', 'pazar']
+const result = document.querySelector('.result')
+
+
+if (weekdays.indexOf(day.toLowerCase()) != -1) {
+    result.style.color = 'yellow'
+    result.style.fontSize = '1.5rem'
+    result.textContent = `${day} bir hafta ici gunudur`
+
+} else if (weekend.indexOf(day.toLowerCase()) != -1) {
+    result.style.color = 'blue'
+    result.style.fontSize = '1.5rem'
+    result.textContent = `${day} bir hafta sonu gunudur`
+} else {
+    result.style.color = 'red'
+    result.style.fontSize = '1.5rem'
+    result.textContent = "Hatali gun girdiniz!!"
+}
+
